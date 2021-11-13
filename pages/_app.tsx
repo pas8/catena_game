275b6d737ce6eb062b6454c533cad33c2cc29db8
pas8 <whyiampas@gmail.com>
2021-11-13 20:48:16 +0200

    added new dependency and functionalty to generate preview ground and of course chnaging  propertyies ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,12 @@ import type { AppProps } from 'next/app';
 import { ComposeLayouts } from '../src/layouts';
 import ProgressLayout from '../src/layouts/ProgressLayout';
 import HeaderLayout from '../src/layouts/HeaderLayout';
+import ToastifyLayout from '../src/layouts/ToastifyLayout';
 
 const _App = ({ Component, pageProps }: AppProps) => {
   return (
-    <div className='dark:bg-dark-42  '>
-      <ComposeLayouts layouts={[ProgressLayout, HeaderLayout]}>
+    <div className='dark:bg-dark-42  min-h-screen'>
+      <ComposeLayouts layouts={[ProgressLayout, HeaderLayout, ToastifyLayout]}>
         <Component {...pageProps} />
       </ComposeLayouts>
     </div>
