b4b908fc93e62fb8bcdbe80adbb0c0b97a709482
pas8 <whyiampas@gmail.com>
2021-11-14 23:18:14 +0200

    changing type of connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ import { useRouter } from 'next/dist/client/router';
 const Board = dynamic(() => import('../src/components/Board'), {});
 
 // export const client = new W3CWebSocket('ws://127.0.0.1:8000 ');
-export const client = new W3CWebSocket('ws://catenagame.herokuapp.com');
+export const client = new W3CWebSocket('wss://catenagame.herokuapp.com');
 
 const Home: NextPage = () => {
   const { reload } = useRouter();
